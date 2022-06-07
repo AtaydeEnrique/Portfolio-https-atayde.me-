0da0867ef80e6725f11670b8962c4dbb6231618f
Atayde Enrique <riorat97@gmail.com>
2022-06-07 13:17:19 -0500

    upgrade to react18

--- src/components/Sections/Skills/SkillBE.js
+++ src/components/Sections/Skills/SkillBE.js
@@ -1,5 +1,5 @@
 import SkillCard from "./SkillCard";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
 
