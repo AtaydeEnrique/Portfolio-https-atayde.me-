6864e7e05ff2baa2f3432619eb58aeaaf126a07a
Atayde Enrique <riorat97@gmail.com>
2022-10-16 22:08:08 -0500

    add faq page

--- src/app6/components/FAQ.js
+++ src/app6/components/FAQ.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import reactLogo from "../../app1/assets/langs/reactfront.svg";
+import githubLogo from "../assets/icons/github.svg";
+import "./FAQ.css";
 function FAQ() {
-  return <div>FAQ</div>;
+  return (
+    <div className="blog-faq">
+      <img className="spin" src={reactLogo} alt="react logo" />
+      <p>Made with React</p>
+      <a
+        href="https://github.com/AtaydeEnrique/Portfolio-https-atayde.me-"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="FAQ-wrapper">
+          <img src={githubLogo} alt="github logo" />
+          <p>Link to this project</p>
+        </div>
+      </a>
+    </div>
+  );
 }
 
 export default FAQ;
